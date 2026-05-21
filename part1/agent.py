19c9cb632aee16c9489479519839538ec3bfe581
funditoress <tangiray03@gmail.com>
2026-05-22 01:49:36 +0200

    uh

--- part1/agent.py
+++ part1/agent.py
@@ -1,106 +1,167 @@
+import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
 
-class PolicyNetwork(nn.Module):
-    def __init__(self, state_dim=11, action_dim=3, hidden_dim=64):
-        super(PolicyNetwork, self).__init__()
-        self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        
-        # Mean and Std for action distribution
-        self.action_mean = nn.Linear(hidden_dim, action_dim)
-        # Standard deviation cannot be negative, so we'll keep log_std and then take exp()
-        self.action_log_std = nn.Parameter(torch.full((1, action_dim), -1.0))
 
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        
-        mean = torch.tanh(self.action_mean(x)) # Hopper actions are typically in the range [-1, 1], so we can use tanh to bound the mean
-        std = torch.exp(self.action_log_std)
-        
-        return mean, std
+def discount_rewards(r, gamma):
+    discounted_r = torch.zeros_like(r)
+    running_add = 0
+    for t in reversed(range(0, r.size(-1))):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
 
-class REINFORCEAgent:
-    def __init__(self, state_dim=11, action_dim=3):
-        self.policy = PolicyNetwork(state_dim, action_dim)
-        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=1e-3)
+
+class Policy(torch.nn.Module):
+    def __init__(self, state_space, action_space):
+        super().__init__()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden = 64
+        self.tanh = torch.nn.Tanh()
+
+        """
+            Actor network
+        """
+        self.fc1_actor = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_actor = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_actor_mean = torch.nn.Linear(self.hidden, action_space)
         
-        # Memory lists to store log probabilities and rewards for each episode
-        self.log_probs = []
+        # Learned standard deviation for exploration at training time 
+        self.sigma_activation = F.softplus
+        init_sigma = 0.5
+        self.sigma = torch.nn.Parameter(torch.zeros(self.action_space) + init_sigma)
+
+        """
+            Critic network
+        """
+        # TASK 3: critic network for actor-critic algorithm
+        self.fc1_critic = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_critic = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_critic_value = torch.nn.Linear(self.hidden, 1)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.normal_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        """
+            Actor
+        """
+        x_actor = self.tanh(self.fc1_actor(x))
+        x_actor = self.tanh(self.fc2_actor(x_actor))
+        action_mean = self.fc3_actor_mean(x_actor)
+
+        sigma = self.sigma_activation(self.sigma)
+        normal_dist = Normal(action_mean, sigma)
+
+        """
+            Critic
+        """
+        # TASK 3: forward in the critic network
+        x_critic = self.tanh(self.fc1_critic(x))
+        x_critic = self.tanh(self.fc2_critic(x_critic))
+        state_value = self.fc3_critic_value(x_critic)
+        
+        return normal_dist
+
+
+class Agent(object):
+    def __init__(self, policy, device='cpu'):
+        self.train_device = device
+        self.policy = policy.to(self.train_device)
+        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
+
+        self.gamma = 0.99
+        self.states = []
+        self.next_states = []
+        self.action_log_probs = []
         self.rewards = []
+        self.done = []
 
-    def select_action(self, state):
-        state = torch.FloatTensor(state)
-        mean, std = self.policy(state)
-        
-        dist = Normal(mean, std)
-        action = dist.sample()
-        
-        # LETS DO THE SQUEEZE TRICK TO AVOID LOG_PROB SHAPE ISSUES
-        # First we sample the action, then we squeeze it to remove any extra dimensions, and finally we calculate the log probability of that action
-        action_squeezed = action.squeeze(0)
-        log_prob = dist.log_prob(action_squeezed).sum(dim=-1)
-        self.log_probs.append(log_prob)
-        
-        return action_squeezed.detach().numpy()
-    
-    def update(self, gamma=0.99):
-            discounted_rewards = []
-            G = 0
+    def update_policy(self, algorithm="REINFORCE_BASELINE", b=20):
+        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)
+        states = torch.stack(self.states, dim=0).to(self.train_device).squeeze(-1)
+        next_states = torch.stack(self.next_states, dim=0).to(self.train_device).squeeze(-1)
+        rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
+        done = torch.Tensor(self.done).to(self.train_device)
+
+        # Clear memory after stacking
+        self.states, self.next_states, self.action_log_probs, self.rewards, self.done = [], [], [], [], []
+
+        if algorithm == "REINFORCE_BASELINE":
+            #
+            # TASK 2: REINFORCE with Stable Baseline = 20
+            #
+            # 1. Compute discounted returns
+            returns = discount_rewards(rewards, self.gamma)
             
-            # Calculate discounted rewards in reverse order (from the end of the episode to the beginning)
-            for r in reversed(self.rewards):
-                G = r + gamma * G
-                discounted_rewards.insert(0, G)
-                
-            discounted_rewards = torch.FloatTensor(discounted_rewards)
+            # 2. Advantage terimini hesapla (G_t - b)
+            advantages = returns - b
             
-            # Normalize discounted rewards for better training stability
-            discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-5)
+            # 3. compute policy gradient loss function given actions and returns
+            policy_loss = -(action_log_probs * advantages).sum()
             
-            policy_loss = []
-            for log_prob, Gt in zip(self.log_probs, discounted_rewards):
-                policy_loss.append(-log_prob * Gt)
-                
-            # Gradient update
+            # 4. Compute gradients and step the optimizer
             self.optimizer.zero_grad()
-            policy_loss = torch.stack(policy_loss).sum()
             policy_loss.backward()
             self.optimizer.step()
+
+        elif algorithm == "ACTOR_CRITIC":
+            #
+            # TASK 3: Actor-Critic
+            #
+            # 1. Compute boostrapped discounted return estimates
+            _, state_values = self.policy(states)
+            state_values = state_values.squeeze(-1)
             
-            # Clear memory
-            self.log_probs = []
-            self.rewards = []
+            with torch.no_grad():
+                _, next_state_values = self.policy(next_states)
+                next_state_values = next_state_values.squeeze(-1)
+                # If done is True, we don't add the next state value to the target
+                target_values = rewards + self.gamma * next_state_values * (1 - done)
             
-    def update2(self, gamma=0.99, b=20): # b=20 baseline
-        discounted_rewards = []
-        G = 0
-        
-        # Calculate discounted rewards in reverse order (from the end of the episode to the beginning)
-        for r in reversed(self.rewards):
-            G = r + gamma * G
-            discounted_rewards.insert(0, G)
+            # 2. Compute advantage terms
+            advantages = target_values - state_values
             
-        discounted_rewards = torch.FloatTensor(discounted_rewards)
-        
-        # Note: We are not normalizing the rewards here because we are using a baseline (b=20) to reduce variance, and the advantage calculation will already help with stability. Normalizing after subtracting the baseline could potentially distort the advantage estimates, so we will skip normalization in this case.
-        
-        policy_loss = []
-        for log_prob, Gt in zip(self.log_probs, discounted_rewards):
-            # Calculating (G_t - b)
-            advantage = Gt - b 
-            policy_loss.append(-log_prob * advantage)
+            # 3. Compute actor loss and critic loss
+            actor_loss = -(action_log_probs * advantages.detach()).sum()
+            critic_loss = F.mse_loss(state_values, target_values, reduction='sum')
             
-        # Gradient update
-        self.optimizer.zero_grad()
-        policy_loss = torch.stack(policy_loss).sum()
-        policy_loss.backward()
-        self.optimizer.step()
-        
-        # Clear memory
-        self.log_probs = []
-        self.rewards = []
+            total_loss = actor_loss + 0.5 * critic_loss
             
+            # 4. Compute gradients and step the optimizer
+            self.optimizer.zero_grad()
+            total_loss.backward()
+            self.optimizer.step()
+
+        return         
+
+    def get_action(self, state, evaluation=False):
+        """ state -> action (3-d), action_log_densities """
+        x = torch.from_numpy(state).float().to(self.train_device)
+
+        normal_dist = self.policy(x)
+
+        if evaluation:  # Return mean
+            return normal_dist.mean, None
+
+        else:   # Sample from the distribution
+            action = normal_dist.sample()
+
+            # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
+            action_log_prob = normal_dist.log_prob(action).sum()
+
+            return action, action_log_prob
+
+    def store_outcome(self, state, next_state, action_log_prob, reward, done):
+        self.states.append(torch.from_numpy(state).float())
+        self.next_states.append(torch.from_numpy(next_state).float())
+        self.action_log_probs.append(action_log_prob)
+        self.rewards.append(torch.Tensor([reward]))
+        self.done.append(done)
